c02180a9d1555b73b5737bafc185db9a783427ba
Isabellagg19 <isabellaguioc123@gmail.com>
2025-10-05 14:15:13 -0500

    Added letter names

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 const input = document.getElementById('input');
+
 const audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+
+const oscillator = audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = 'sine';
+
+oscillator.start();
+gainNode.gain.value = 0;
+
+const notenames = new Map();
+notenames.set('A', 261.63);
+notenames.set('B',293.66);
+notenames.set('C', 329.63);
+notenames.set('D', 349.23);
+notenames.set('E', 392.00);
+notenames.set('F', 440.00);
+notenames.set('G', 493.88);
 
 function frequency(pitch) {
-  const freq = parseFloat(pitch); 
-  if (isNaN(freq)) {
-    console.log("Not a valid frequency");
-    return;
-  }
-
-  const gainNode = audioCtx.createGain();
-  const oscillator = audioCtx.createOscillator();
-
-  oscillator.type = "sine";
-  oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime);
-
-  oscillator.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-
-  gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
+  if (!pitch) return;
+    const freq = Number(pitch);
+  if (isNaN(freq)) return;
+  
+  gainNode.gain.setValueAtTime(0.2, audioCtx.currentTime);
+  oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
-
-  oscillator.start();
-  oscillator.stop(audioCtx.currentTime + 1);
 }
 
 function handle() {
   audioCtx.resume();
-  frequency(input.value);
+  var usernotes = String(input.value.toUpperCase());
+  const pitch = notenames.get(usernotes);
+  frequency(pitch);
 }
